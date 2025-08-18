93372f373a776cf7e4d1b924a5a4792b92e25c15
AgusSantarena <santarenaagustin69@gmail.com>
2025-08-18 23:41:07 +0000

    Clase 1 JavaScript

---
+++ Python/Clase1/Ejercicios/Ejercicio2.py
@@ -0,0 +1,8 @@
+# Eiercicio 2: Crear un rango de numeros de 2 a 6 a imprimelos
+# Eiemplo de ejecución: 2,3,4,5,6
+
+
+Lista = [2, 3, 4, 5, 6]
+
+for num in Lista:
+   print(num)
---
+++ Python/Clase1/Listas1.py
@@ -0,0 +1,10 @@
+# listas = Ariel, Liliana, Agustin, Gabriel
+
+nombres = ["Agustin", "Gabriel", "Lili", "Ariel"]
+print(nombres)
+
+print[nombres[0]]
+print[nombres[1]]
+
+print[nombres[-1]]
+print[nombres[-2]]
